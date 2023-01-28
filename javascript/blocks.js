c4fa48cc42c65b1f5760dc823bb6a3a3cda5528f
nddszy <1836107093@qq.com>
2023-01-28 20:37:27 +0800

    Update
    第一次的H5小游戏算是开发完成了，坏消息是生成积木题面的算法依然有瑕疵，有时会生成无解的题面，好消息是，几乎碰不到。

--- javascript/blocks.js
+++ javascript/blocks.js
@@ -2,15 +2,23 @@
 js是世界上最垃圾的语言！！！
 没有之一！！！
 canvas坐标轴：
-0,0 1,0 2,0 ...
+0,0 1,0 2,0 x,y...
 0,1 1,1 2,1
 0,2 1,2 2,2
 ...
+js数组坐标轴：
+0,0 0,1 0,2 x,y...
+1,0 1,1 1,2
+2,0 2,1 2,2
+...
 */
-var c = document.getElementById("canvas");
+const c = document.getElementById("canvas");
 //找到 <canvas> 元素
-var ctx = c.getContext("2d");
+const ctx = c.getContext("2d");
 //getContext("2d") 对象是内建的 HTML5 对象，拥有多种绘制路径、矩形、圆形、字符以及添加图像的方法
+const squareLength = 120; //单位方块的边长（像素）
+const xCount = 5; //棋盘有多少列
+const yCount = xCount; //棋盘有多少行
 
 window.onload = function () {
   Start();
@@ -32,14 +40,14 @@ const directionEnum = {
 } //方向枚举，四个成员是方向
 
 class block {
-  constructor(squares, direction) {
+  constructor(squares, direction, color) {
     this.squares = squares; //积木包含的单位方块数组（一块积木由一个或多个单位方块构成）
     this.direction = direction;
+    this.color = color;
   }
 } //积木类
 
 var occupiedMap = []; //占位棋盘，用来标注棋盘任一个方块所属的积木
-//var curBlocks = []; //场上当前积木数组
 var occupiedSquareCount = 0; //场上被积木占据的方块数量
 
 function Start() {
@@ -48,17 +56,18 @@ function Start() {
 } //全局开始
 
 function StartNewGame() {
-  var direcMap = BuildDirectionMap(10, 10);
+  var direcMap = BuildDirectionMap(xCount, yCount);
   var blocks = BuildBlocks(direcMap);
-  BuildoccupiedMap(blocks, 10, 10);
+  occupiedSquareCount = xCount * yCount;
+  BuildoccupiedMap(blocks, xCount, yCount);
   DrawMap(blocks);
 } //开始一局新游戏
 
 function GameEnd() {
-
+  StartNewGame();
 } //游戏结束时的函数
 
-function BuildoccupiedMap(blocks, xCount, yCount) {
+function BuildoccupiedMap(blocks) {
   var arr = new Array(xCount); //表格的行数
   for (var i = 0; i < arr.length; i++) {
     arr[i] = new Array(yCount); //表格的列数
@@ -73,9 +82,10 @@ function BuildoccupiedMap(blocks, xCount, yCount) {
     }
   }
   occupiedMap = arr;
+  //console.log(occupiedMap);
 } //构建占位棋盘
 
-function BuildDirectionMap(xCount, yCount) {
+function BuildDirectionMap() {
   var arr = new Array(xCount); //表格的行数
   for (var i = 0; i < arr.length; i++) {
     arr[i] = new Array(yCount); //表格的列数
@@ -141,13 +151,38 @@ function BuildDirectionMap(xCount, yCount) {
 function BuildBlocks(directMap) {
   var blocks = [];
   var blockIndex = 0;
+  var curDirection;
   for (var i = 0; i < directMap.length; i++) {
     for (var j = 0; j < directMap[0].length; j++) {
-      blocks[blockIndex] = new block([new square(j, i)], directMap[i][j]);
+      if (!directMap[i][j]) continue;
+      //如果当前位置上的方块已经属于其他积木了，就跳过
+      var curSquares = [];
+      curSquares[0] = new square(j, i);
+      curDirection = directMap[i][j];
+      if (j < xCount - 1 && directMap[i][j + 1] == curDirection) {
+        curSquares[1] = new square(j + 1, i);
+        directMap[i][j + 1] = null;
+        if (i < yCount - 1 && directMap[i + 1][j] == curDirection &&
+          directMap[i + 1][j + 1] == curDirection) {
+          curSquares[2] = new square(j, i + 1);
+          curSquares[3] = new square(j + 1, i + 1);
+          directMap[i + 1][j] = null;
+          directMap[i + 1][j + 1] = null;
+        }
+      }
+      else {
+        if (i < yCount - 1 && directMap[i + 1][j] == curDirection) {
+          curSquares[1] = new square(j, i + 1);
+          directMap[i + 1][j] = null;
+        }
+      }
+      var color = RandomRgbColor();
+      //生成一个随机颜色（#FFFFFF这种Hex颜色）
+      blocks[blockIndex] = new block(curSquares, curDirection, color);
       blockIndex++;
     }
   }
-  //console.log(blocks);i
+  //console.log(blocks);
   return blocks;
 } //通过方向棋盘构建场上积木
 
@@ -157,30 +192,27 @@ function DrawMap(blocks) {
   }
 } //绘制棋盘（所有积木）
 
-function DrawBlock(block, length) {
+function DrawBlock(block) {
   var minX = block.squares[0].x, minY = block.squares[0].y,
+    maxX = block.squares[0].x, maxY = block.squares[0].y,
     xCount = 1, yCount = 1;
   for (var i = 1; i < block.squares.length; i++) {
-    if (block.squares[i].x <= minX) {
-      minX = block.squares[i].x;
-    }
-    else {
-      xCount++;
-    }
-    if (block.squares[i].y <= minY) {
-      minY = block.squares[i].y;
-    }
-    else {
-      yCount++;
-    }
+    minX = block.squares[i].x < minX ? block.squares[i].x : minX;
+    maxX = block.squares[i].x > maxX ? block.squares[i].x : maxX;
+    minY = block.squares[i].y < minY ? block.squares[i].y : minY;
+    maxY = block.squares[i].y > maxY ? block.squares[i].y : maxY;
   }
-  var xLength = xCount * length, yLength = yCount * length,
-    xPoint = minX * length, yPoint = minY * length;
-  ctx.fillStyle = "#FFFFFF";
-  ctx.strokeStyle = "#000000";
+  xCount += maxX - minX;
+  yCount += maxY - minY;
+  var xLength = xCount * squareLength, yLength = yCount * squareLength,
+    xPoint = minX * squareLength, yPoint = minY * squareLength;
+  ctx.fillStyle = block.color;
+  //ctx.fillStyle = "#FFFFFF";
+  //ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  //ctx.strokeStyle = "#000000";
   ctx.fillRect(xPoint, yPoint, xLength, yLength);
-  ctx.strokeRect(xPoint, yPoint, xLength, yLength);
-  var fontLength = Math.round(length / 2);
+  //ctx.strokeRect(xPoint, yPoint, xLength, yLength);
+  var fontLength = Math.round(squareLength / 2);
   DrawArrow(xPoint + xLength / 2, yPoint + yLength / 2, block.direction, fontLength);
 } //绘制积木（注意，矩形的原点在左上角）
 
@@ -194,41 +226,36 @@ function DeleteBlock(block) {
   }
 } //删除积木
 
-function ClearBlock(block, length) {
+function ClearBlock(block) {
   var minX = block.squares[0].x, minY = block.squares[0].y,
+    maxX = block.squares[0].x, maxY = block.squares[0].y,
     xCount = 1, yCount = 1;
   for (var i = 1; i < block.squares.length; i++) {
-    if (block.squares[i].x <= minX) {
-      minX = block.squares[i].x;
-    }
-    else {
-      xCount++;
-    }
-    if (block.squares[i].y <= minY) {
-      minY = block.squares[i].y;
-    }
-    else {
-      yCount++;
-    }
+    minX = block.squares[i].x < minX ? block.squares[i].x : minX;
+    maxX = block.squares[i].x > maxX ? block.squares[i].x : maxX;
+    minY = block.squares[i].y < minY ? block.squares[i].y : minY;
+    maxY = block.squares[i].y > maxY ? block.squares[i].y : maxY;
   }
-  var xLength = xCount * length, yLength = yCount * length,
-    xPoint = minX * length, yPoint = minY * length;
+  xCount += maxX - minX;
+  yCount += maxY - minY;
+  var xLength = xCount * squareLength, yLength = yCount * squareLength,
+    xPoint = minX * squareLength, yPoint = minY * squareLength;
   ctx.clearRect(xPoint, yPoint, xLength, yLength);
 } //清除积木图像
 
-function DrawArrow(x, y, direction, length) {
+function DrawArrow(x, y, direction, fontLength) {
   ctx.fillStyle = "#000000";
-  ctx.font = length.toString() + "px Arial";
-  ctx.fillText(direction, x - length / 2, y + length / 2);
+  ctx.font = fontLength.toString() + "px Arial";
+  ctx.fillText(direction, x - fontLength / 2, y + fontLength / 2);
 } //绘制箭头（注意，文字的原点在左下角）
 
-function BlockCanMove(block, xCount, yCount) {
+function BlockCanMove(block) {
   if (!block) return false;
   var curSquare;
   for (var i = 0; i < block.squares.length; i++) {
     curSquare = block.squares[i];
     if (block.direction == directionEnum.up) {
-      if (curSquare.y - 1 > 0 &&
+      if (curSquare.y > 0 &&
         occupiedMap[curSquare.x][curSquare.y - 1] &&
         occupiedMap[curSquare.x][curSquare.y - 1] != block) {
         return false;
@@ -242,7 +269,7 @@ function BlockCanMove(block, xCount, yCount) {
       }
     }
     else if (block.direction == directionEnum.left) {
-      if (curSquare.x - 1 > 0 &&
+      if (curSquare.x > 0 &&
         occupiedMap[curSquare.x - 1][curSquare.y] &&
         occupiedMap[curSquare.x - 1][curSquare.y] != block) {
         return false;
@@ -259,62 +286,71 @@ function BlockCanMove(block, xCount, yCount) {
   return true;
 } //判断积木是否可以移动
 
-function MoveBlock(block, xCount, yCount, length) {
-  ClearBlock(block, length);
+function MoveBlock(block) {
+  ClearBlock(block);
   var curSquare;
+  var lastSquares = [];
+  var nowSquares = [];
   for (var i = 0; i < block.squares.length; i++) {
     curSquare = block.squares[i];
-    occupiedMap[curSquare.x][curSquare.y] = null;
+    lastSquares[i] = { x: curSquare.x, y: curSquare.y };
     if (block.direction == directionEnum.up) {
       if (curSquare.y == 0) {
         DeleteBlock(block);
         return;
-      }
-      curSquare.y -= 1;
+      }//如果当前方块已经到顶，说明移动后将移出棋盘，则直接删除方块
+      nowSquares[i] = { x: curSquare.x, y: curSquare.y - 1 };
     }
     else if (block.direction == directionEnum.down) {
       if (curSquare.y == yCount - 1) {
         DeleteBlock(block);
         return;
       }
-      curSquare.y += 1;
+      nowSquares[i] = { x: curSquare.x, y: curSquare.y + 1 };
     }
     else if (block.direction == directionEnum.left) {
       if (curSquare.x == 0) {
         DeleteBlock(block);
         return;
       }
-      curSquare.x -= 1;
+      nowSquares[i] = { x: curSquare.x - 1, y: curSquare.y };
     }
     else if (block.direction == directionEnum.right) {
       if (curSquare.x == xCount - 1) {
         DeleteBlock(block);
         return;
       }
-      curSquare.x += 1;
+      nowSquares[i] = { x: curSquare.x + 1, y: curSquare.y };
     }
-    occupiedMap[curSquare.x][curSquare.y] = block;
-  }
-  DrawBlock(block, length);
+  }//分别将积木内的每一个方块的坐标改成移动后的，同时判断此积木是否能直接移除
+  for (var i = 0; i < lastSquares.length; i++) {
+    occupiedMap[lastSquares[i].x][lastSquares[i].y] = null;
+  }//将占位棋盘中积木原先的地方置空
+  for (var i = 0; i < block.squares.length; i++) {
+    occupiedMap[nowSquares[i].x][nowSquares[i].y] = block;
+  }//将占位棋盘中积木新的位置上写入此积木
+  block.squares = nowSquares;//将有了新的位置的方块组赋给此积木
+  DrawBlock(block);
 } //移动积木
 
 function BindCanvasClickEvent(canvas) {
   canvas.addEventListener('click', function (e) {
-    pos = getEventPosition(e); //点击的坐标
-    //console.log(BlockCanMove(GetBlockByPosition(pos.x, pos.y, 60), 10, 10));
-    //ClearBlock(GetBlockByPosition(pos.x, pos.y, 60), 60);
+    pos = GetEventPosition(e); //点击的坐标
     curBlock = GetBlockByPosition(pos.x, pos.y, 60);
-    if (BlockCanMove(curBlock, 10, 10)) {
-      MoveBlock(curBlock, 10, 10, 60);
+    canMove = BlockCanMove(curBlock, xCount, yCount);
+    console.log(canMove);
+    console.log(occupiedMap);
+    if (canMove) {
+      MoveBlock(curBlock, xCount, yCount, 60);
     }
   })
 } //为canvas绑定点击事件
 
-function GetBlockByPosition(xPos, yPos, length) {
-  return occupiedMap[Math.floor(xPos / length)][Math.floor(yPos / length)];
+function GetBlockByPosition(xPos, yPos) {
+  return occupiedMap[Math.floor(xPos / squareLength)][Math.floor(yPos / squareLength)];
 } //通过坐标获取积木
 
-function getEventPosition(ev) {
+function GetEventPosition(ev) {
   var x, y;
   if (ev.layerX || ev.layerX == 0) {
     x = ev.layerX;
@@ -325,5 +361,21 @@ function getEventPosition(ev) {
   }
   return { x: x, y: y };
 } //获取点击canvas时的坐标
-  //注：使用这个函数，需要给Canvas元素的position设为absolute。
-  //要判断事件对象发生的位置，事件对象ev的layerX和layerY属性表示Canvas内部坐标系中的坐标
+//注：使用这个函数，需要给Canvas元素的position设为absolute。
+//要判断事件对象发生的位置，事件对象ev的layerX和layerY属性表示Canvas内部坐标系中的坐标
+
+function RandomRgbColor() {
+  let arr = [];
+  for (var i = 0; i < 3; i++) {
+    // 暖色
+    //arr.push(Math.floor(Math.random() * 128 + 64));
+    // 亮色
+    arr.push(Math.floor(Math.random() * 128 + 128));
+  }
+  let [r, g, b] = arr;
+  // rgb颜色
+  // var color=`rgb(${r},${g},${b})`;
+  // 16进制颜色
+  var color = `#${r.toString(16).length > 1 ? r.toString(16) : '0' + r.toString(16)}${g.toString(16).length > 1 ? g.toString(16) : '0' + g.toString(16)}${b.toString(16).length > 1 ? b.toString(16) : '0' + b.toString(16)}`;
+  return color;
+}//随机生成RGB颜色（亮色）
